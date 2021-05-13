d75a8f38b46a096b583efc2b4df0ac95438f822b
Hanneke <hanneke3108@gmail.com>
2021-05-13 17:46:18 +0200

    Changed rotation
    Changed rotation.

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -3,8 +3,8 @@ var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
