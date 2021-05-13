2158103b7738cfa58fe29c921b76645c28ca0900
Hanneke <hanneke3108@gmail.com>
2021-05-13 12:54:20 +0200

    Changed clockwise
    Changed clockwise.

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -3,8 +3,8 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.z += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.z);
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 60); //equivalent to 60 fps
