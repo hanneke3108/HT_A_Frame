3e44c2531b4ccfdba489d1312223c8d44d9c01f2
Hanneke <hanneke3108@gmail.com>
2021-05-13 17:31:06 +0200

    Changed rotation
    Changed rotation.

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,10 +1,11 @@
 // changing individual properties with code and using setInterval
 var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
+var axis = new THREE.Vector3(4, 0, 7);
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.x);
+	console.log(myOtherBox.object3D.rotationOnAxis(axis, rotationSpeed));
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
